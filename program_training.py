8afacd88bea420af518794ff553dcf2b5f3a579e
ameesh <ars7@rice.edu>
2022-04-04 19:25:47 -0700

    separating labels and data

--- program_training.py
+++ program_training.py
@@ -9,7 +9,8 @@ from tqdm import tqdm
 
 
 def run_intersection_world(data_collection=False):
-    state_action_tuples = []
+    states = []
+    actions = []
     # instantiate world
     wenv = IntersectionScenario()
     if not data_collection:
@@ -19,20 +20,29 @@ def run_intersection_world(data_collection=False):
         a = None
         o, r, d, info = wenv.step(a)
         if data_collection:
-            state_action_tuples.append(info["previous_state_action"])
+            state, action = info["previous_state_action"]
+            states.append(state)
+            actions.append(action)
         else:
             wenv.render()
 
-    return np.array(state_action_tuples)
+    return np.array(states), np.array(actions)
 
-def collect_data_for_imititation_learning(experiment_method, num_runs=500, outfilepath=None):
-    data_set = []
+def collect_data_for_imititation_learning(experiment_method, num_runs=50, outfilepath=None):
+    state_data = []
+    action_data = []
     print("beginning data collection...")
     for _ in tqdm(range(num_runs)):
-        state_action_tuples = experiment_method(data_collection=True)
-        data_set.extend(state_action_tuples)
-    with open(outfilepath, "wb") as outfle:
-        np.save(outfle, data_set)
-        print("saved data to {}".format(outfilepath))
+        states, actions = experiment_method(data_collection=True)
+        state_data.extend(states)
+        action_data.extend(actions)
+    statefilepath = "{}_states".format(outfilepath)
+    actionfilepath = "{}_actions".format(outfilepath)
+    with open(statefilepath, "wb") as state_outfle:
+        np.save(state_outfle, state_data)
+        print("saved data to {}".format(statefilepath))
+    with open(actionfilepath, "wb") as action_outfle:
+        np.save(action_outfle, action_data)
+        print("saved data to {}".format(statefilepath))
 
 collect_data_for_imititation_learning(experiment_method=run_intersection_world, num_runs=10, outfilepath="intersectionsimple_test")
